4bf6bff3649fb6aabf97df77addec3219d67808b
Jackelin12 <125749432+Jackelin12@users.noreply.github.com>
2023-03-01 21:25:22 -0600

    Código completo

---
+++ Laboratorio1.JackelinMariaAguilarTobar/Negocio/Cls Persona.cs
@@ -0,0 +1,38 @@
+﻿using Laboratorio1.JackelinMariaAguilarTobar.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1.JackelinMariaAguilarTobar.Negocio
+{
+    public class ClsPersona
+    {
+        public String CalcularIMC(Persona per)
+        {
+            double Promedio = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (Promedio > 25)
+            {
+                return $"tiene sobre peso";
+            }
+            else
+            {
+                return $"tiene un peso ideal";
+            }
+        }
+
+        public String MayorDeEdad(Persona per)
+        {
+            if (per.Edad >= 18)
+            {
+                return $"Es mayor de edad";
+            }
+            else
+            {
+                return $"Es menor de edad";
+            }
+        }
+    }
+}
---
+++ Laboratorio1.JackelinMariaAguilarTobar/Program.cs
@@ -0,0 +1,30 @@
+﻿using Laboratorio1.JackelinMariaAguilarTobar.Negocio;
+using Laboratorio1.JackelinMariaAguilarTobar.Entidades;
+
+
+Persona per = new Persona();
+ClsPersona clsPersona = new ClsPersona();
+
+Console.Write(@"Bienvenido a su calculador de IMC
+Ingrese los siguientes datos para continuar 
+Ingrese su Nombre -> ");
+per.Nombre = Console.ReadLine();
+
+Console.Write("\nIngrese su edad -> ");
+per.Edad = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nIngrese su sexo -> ");
+per.Sexo = Console.ReadLine();
+
+Console.Write("\nIngrese su peso en Kg. -> ");
+per.Peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\nIngrese su altura en metros -> ");
+per.Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($@"
+Nombre: {per.Nombre} edad: {per.Edad} sexo: {per.Sexo}
+Peso: {per.Peso}
+Altura: {per.Altura}
+Usted {clsPersona.CalcularIMC(per)}
+Usted {clsPersona.MayorDeEdad(per)}");
